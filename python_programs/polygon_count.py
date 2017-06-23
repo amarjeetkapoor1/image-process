197574a4d2a8777fdc1cce8feae8b4cd5e0c8cc2
amarjeetkapoor1 <amarjeet.kapoor1@gmail.com>
2017-06-23 21:07:00 +0530

    removed extra code

--- python_programs/polygon_count.py
+++ python_programs/polygon_count.py
@@ -1,68 +1,76 @@
 import numpy as np
 import cv2
-from PIL import Image
-from matplotlib import pyplot as plt
 import sys
 
-def getshape(binary,orignal):
-	global point_contour
-	point_contour =[]
-	i, contours, hierarchy = cv2.findContours(binary,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-	x,y=binary.shape
-	bl= np.zeros((x,y,3), np.uint8)
-	for a in contours :
-		epsilon = 0.02*cv2.arcLength(a,True)
-		approx = cv2.approxPolyDP(a,epsilon,True)
-		point_contour.append(approx)
-		x,y=binary.shape
-		i=255*np.random.random_sample()
-		cv2.drawContours(bl,[a],0, (int(i),int(i),int(i)), -1)
-		cv2.drawContours(bl,[a],0, (int(0),int(0),int(0)), 3)
-	return bl,contours
-	
-	
+"""
+Time complexity depends upon shape of image
+
+"""
+
+def getfilledpoly(binary):
+
+    #Get contours
+    not_used, contours, hierarchy = cv2.findContours(binary,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+
+    #Get shape of binary image to create a blank image of same size
+    x, y = binary.shape
+    filledPolygon = np.zeros((x,y,3), np.uint8)
+
+    #Fill the polygons
+    color = 0
+
+    #Iterat over all contoures starting from innermost
+    for cont in contours :
+
+        #Check color to  be used is range
+        if(color>=255):
+            color=0
+
+        #Increment color or change color
+        color=color+1
+
+        #Fill the contour with given color
+        cv2.drawContours(filledPolygon,[cont],0, (int(color),int(color),int(color)), -1)
+
+        #Draw border of contour and this is important if the polygon is open
+        #Its previous color used to fill area will be replaced with (0,0,0)
+        #as area of contour will be equal to its border
+        cv2.drawContours(filledPolygon,[cont],0, (int(0),int(0),int(0)), 3)
+
+    return filledPolygon
+
+
 if __name__=='__main__':
     if len(sys.argv)==2:
+
+        #Get Image
         im = cv2.imread(sys.argv[1])
+
+        #Convert to gray scale
         imgray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-        #canny edge detection is not as useful as threshold_binary
-        edges = cv2.Canny(imgray,100,200)
-        ret, thresh = cv2.threshold(imgray,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
 
+        #Get binary image
+        no_use, thresh = cv2.threshold(imgray,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+
+        #Get image where polygons are filled
+        filled_polygon = getfilledpoly(thresh)
+        cv2.imwrite('filledpolygon.png',filled_polygon)
+
+        #For safty remove holes
         kernel = np.ones((5,5), np.uint8)
+        filled_polygon = cv2.erode(filled_polygon, kernel, iterations=3)
+        filled_polygon = cv2.cvtColor(filled_polygon, cv2.COLOR_BGR2GRAY)
 
-        """
-        img_d = cv2.dilate(edges, kernel, iterations=1)
-        kernel = np.ones((4,4), np.uint8)
-        img_d = cv2.erode(img_d, kernel, iterations=1)
-        """
-
-        a, b = getshape(thresh,im)
-
-        a = cv2.erode(a, kernel, iterations=3)
-        a = cv2.cvtColor(a, cv2.COLOR_BGR2GRAY)
-        #print count_colors.count_color(a)
-        x,y=a.shape
+        #find size of matrix of image 
+        row, column=filled_polygon.shape
         color=[]
-        tr = -1
-        count=0
-        for r in range(x) :
-	        for b in range(y):
-		        color.append(a[r][b])
-	
-        color.sort()
 
-        b=0
+        #Count number of unique colors
+        for rowPixel in range(row) :
+            for columnPixel in range(column):
+                if filled_polygon[rowPixel][columnPixel] not in color:
+                    color.append(filled_polygon[rowPixel][columnPixel])
 
-        for r in color:
-	        if(tr!=r):
-		        count=count+1
-		        tr=r
-		
-        print "No of polygon",count-1
+        print "No of polygon",len(color)-1
 
-
-        cv2.imwrite('na.png',a)
-        cv2.imwrite('na1.png',thresh)
-        #cv2.imwrite('ne.png',edges)
-        #cv2.imwrite('nd.png',img_d)
+        cv2.imwrite('imageAfterthreshing.png',thresh)
